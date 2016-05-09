74d74ac833d97623ba576d4c223aed7873bca36e
Artwalk <frank.holy.art@gmail.com>
2016-05-09 22:14:45 +0800

    Init

---
+++ UIDynamics/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIDynamics
+//
+//  Created by Art on 5/9/16.
+//  Copyright © 2016 Art. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
